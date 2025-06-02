75bc0e0b1d37c7a588076409740f53c6c7f3600a
anveloper <hitedin@gmail.com>
2025-06-02 23:43:15 +0900

    feat: Lighthouse 권장사항 수행

--- js/products.js
+++ js/products.js
@@ -1,7 +1,9 @@
 async function loadProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
-  const products = await response.json();
-  displayProducts(products);
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const products = await response.json();
+    displayProducts(products);
+  } catch (e) {}
 }
 
 function displayProducts(products) {
